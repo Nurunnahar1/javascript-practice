4c1d2cdfda59cf12565129ca21bba85b9eeb98f7
Nurunnahar <nurunnahar9081@gmail.com>
2023-08-19 00:42:13 +0600

    initial commit

---
+++ javascript practice/main.js
@@ -0,0 +1,60 @@
+//1...*************************************** */
+                // function printProfile(){
+                //     console.log("My name is Nasrin");
+                //     console.log("I am 28 years old");
+                //     console.log("I am software Developer ");
+                //     console.log("I love coding and programming");
+                // }
+
+                // function multiply(){
+                //     console.log(5+10);
+                // }
+                // multiply()
+//2...*************************************** */
+
+                // function square(number){
+                //     console.log(number*number);
+                // }
+                // square(5);
+
+                // function area(length,width){
+                //     console.log(length*width);
+                // }
+                // area(5,9);
+
+                // function Plus(number1,number2){
+                //     console.log(number1+number2);
+                // }
+                // Plus(5,9);
+//3...*************************************** */
+
+                // function plus(number1,number2,number3){
+                //     return number1+number2+number3;
+                // }
+                //  const result= plus(1,2,3);
+                // console.log(result+10+25);
+
+
+
+
+                // function printProfile(name, age, profession, hobby) {
+                //   const profile = `My name is ${name}. I am ${age} years old. I am ${profession}. I love ${hobby}`;
+
+                //   return profile;
+                // }
+                // console.log(printProfile("Nasrin", 28, "Software Developer", "coding"));
+                // console.log(printProfile("Muskan", 28, "Designar", "teaching"));
+
+
+
+//3...*************************************** */
+
+const profile={
+    name: "Nasrin",
+    age: 28,
+    fullName:function() {
+        return this.name + " " + this.age
+    }
+}
+console.log(profile.name);
+console.log(profile.fullName());
