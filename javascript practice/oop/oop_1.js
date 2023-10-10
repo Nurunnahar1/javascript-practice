253f51f0e8d6ccb22e4bd0e5731837bb5ecdb06b
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-10 21:08:19 +0600

    constructor & this keyword

---
+++ javascript practice/oop/oop_1.js
@@ -0,0 +1,52 @@
+// function Person(name, age, profession ,date_of_birth){
+//     this.name = name;
+//     this.what_i_do = profession;
+//     this.age = age;
+//     // this.birthday = new Date(date_of_birth) ;
+//     this.birthday = new Date(date_of_birth).toString() ;
+// }
+
+// const Person1 = new Person("Nasrin","28","developer","11-10-1997");
+// const Person2 = new Person("Nurunnahar");
+
+// console.log(Person1,Person2);
+
+//*********find out age* wrong ***********/
+// function Person(name, date_of_birth) {
+//   this.name = name;
+//   this.birthday = new Date(date_of_birth);
+//   this.ageCalculate = () => {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const age = new Date(diff);
+
+//     return age.getFullYear() - 1997;
+//   };
+// }
+
+// const Person1 = new Person("Nasrin", "1997-11-10");
+
+// console.log(Person1.ageCalculate());
+
+function Person(name, date_of_birth) {
+  this.name = name;
+  this.birthday = new Date(date_of_birth);
+  this.ageCalculate = () => {
+    const today = new Date();
+    const age = today.getFullYear() - this.birthday.getFullYear();
+
+    // Check if the birthday for this year has occurred or not
+    if (
+      today.getMonth() < this.birthday.getMonth() ||
+      (today.getMonth() === this.birthday.getMonth() &&
+        today.getDate() < this.birthday.getDate())
+    ) {
+      return age - 1; // Subtract 1 year if the birthday hasn't occurred yet this year
+    }
+
+    return age;
+  };
+}
+
+const Person1 = new Person("Nasrin", "1997-11-10");
+
+console.log(Person1.ageCalculate());
