77224b0f1df7eb9bc7010b87a3168f46fb364f36
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-11 22:37:34 +0600

    static method

--- javascript practice/oop/oop_2.js
+++ javascript practice/oop/oop_2.js
@@ -1,21 +1,27 @@
-class Car{
-    constructor(brand){
-        this.brand = brand;
-    }
-    getInfo = ()=>{
-        return 'This car brand name is ' + this.brand
-    }
+class Car {
+  constructor(brand) {
+    this.brand = brand;
+  }
+  getInfo = () => {
+    return "This car brand name is " + this.brand;
+  };
 }
 
-class CarModel extends Car{
-    constructor(brand, model){
-        super(brand);
-        this.model = model;
-    }
-    printInfo = ()=>{
-        console.log(this.getInfo() + ' and its model number is ' + this.model);
-    }
+class CarModel extends Car {
+  constructor(brand, model) {
+    super(brand);
+    this.model = model;
+  }
+  printInfo = () => {
+    console.log(this.getInfo() + " and its model number is " + this.model);
+  };
+
+  static getFullModelName = () => {
+    return "This is static method";
+  };
 }
 
-const mazda = new CarModel('Mazda', 'RX-8');
- mazda.printInfo();
+const mazda = new CarModel("Mazda", "RX-8");
+mazda.printInfo();
+
+console.log(CarModel.getFullModelName());   //Object will not work class name will be needed
