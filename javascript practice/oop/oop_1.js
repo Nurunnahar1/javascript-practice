ed031b35384caf027be0c869256348d3093d8da2
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-11 21:34:43 +0600

    Class, methods and constructor

--- javascript practice/oop/oop_1.js
+++ javascript practice/oop/oop_1.js
@@ -27,26 +27,79 @@
 
 // console.log(Person1.ageCalculate());
 
-function Person(name, date_of_birth) {
-  this.name = name;
-  this.birthday = new Date(date_of_birth);
-  this.ageCalculate = () => {
-    const today = new Date();
-    const age = today.getFullYear() - this.birthday.getFullYear();
+//*************************find out age  start************************/
 
-    // Check if the birthday for this year has occurred or not
-    if (
-      today.getMonth() < this.birthday.getMonth() ||
-      (today.getMonth() === this.birthday.getMonth() &&
-        today.getDate() < this.birthday.getDate())
-    ) {
-      return age - 1; // Subtract 1 year if the birthday hasn't occurred yet this year
+// function Person(name, date_of_birth) {
+// this.name = name;
+// this.birthday = new Date(date_of_birth);
+// this.ageCalculate = () => {
+//     const today = new Date();
+//     const age = today.getFullYear() - this.birthday.getFullYear();
+
+//     // Check if the birthday for this year has occurred or not
+//     if (
+//     today.getMonth() < this.birthday.getMonth() ||
+//     (today.getMonth() === this.birthday.getMonth() &&
+//         today.getDate() < this.birthday.getDate())
+//     ) {
+//     return age - 1; // Subtract 1 year if the birthday hasn't occurred yet this year
+//     }
+
+//     return age;
+// };
+// }
+
+// const Person1 = new Person("Nasrin", "1997-11-10");
+
+// console.log(Person1.ageCalculate());
+//*****************************find out age  end***************************/
+
+//*****************************find out age  start***************************/
+ 
+
+
+
+class Person {
+    constructor(firstname, lastname, date_of_birth, yearly_income) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.birthday = new Date(date_of_birth);
+      this.yearly_income = yearly_income;
     }
-
-    return age;
-  };
-}
-
-const Person1 = new Person("Nasrin", "1997-11-10");
-
-console.log(Person1.ageCalculate());
+  
+    ageCalculate() {
+      const today = new Date();
+      const age = today.getFullYear() - this.birthday.getFullYear();
+      
+      // Check if the birthday for this year has occurred or not
+      if (
+        today.getMonth() < this.birthday.getMonth() ||
+        (today.getMonth() === this.birthday.getMonth() && today.getDate() < this.birthday.getDate())
+      ) {
+        return age - 1; // Subtract 1 year if the birthday hasn't occurred yet this year
+      }
+      
+      return age;
+    }
+  
+    getFullName() {
+      return this.firstname + " " + this.lastname;
+    }
+  
+    getTaxAmount() {
+      let taxamount = 0;
+      if (this.yearly_income > 250000) {
+        taxamount = this.yearly_income * 0.25;
+      } else if (this.yearly_income > 550000 && this.yearly_income <= 750000) {
+        taxamount = this.yearly_income * 0.3;
+      } else if (this.yearly_income > 750000) {
+        taxamount = this.yearly_income * 0.35;
+      }
+  
+      return taxamount;
+    }
+  }
+  
+  const Pramanik = new Person("Nasrin", "Pramanik", "1997-11-10", 7500000);
+  console.log(Pramanik.getFullName(), Pramanik.ageCalculate(), Pramanik.getTaxAmount());
+  
