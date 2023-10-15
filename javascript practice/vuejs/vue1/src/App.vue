3049e3286d01b80486c0c34e353b545965b29b5a
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-15 18:28:57 +0600

    option api

--- javascript practice/vuejs/vue1/src/App.vue
+++ javascript practice/vuejs/vue1/src/App.vue
@@ -1,14 +1,36 @@
- <template>
-    <h1>hello</h1>
-  <!-- <Homepage></Homepage> -->
-  <Homepage/> 
-  
-    
+<template>
+  <h1>Hello World</h1>
+  <h1>{{ 2+2 }}</h1>
+  <h1>{{ new Date().getDate() }}</h1> <!--//todays date--> 
 
+
+  <h1>{{ first_name }}</h1>
+  <h1>{{ last_name }}</h1>
+
+  <h3>{{ num1+num2 }}</h3>
+
+
+   <button @click="getFullName">Click</button>
  </template>
 
- <script setup>
-import Homepage from './pages/Homepage.vue';
- 
-</script>
+
+ <script  >
+  export default {
+    data(){
+      return{
+        first_name:"Bill",
+        last_name:"Gates",
+        age:65,
+
+        num1:10,
+        num2:20,
+
+        getFullName:()=>{
+          alert("Hello ")
+        }
+      }
+    }
+  }
+ </script>
+
  <style scoped></style>
