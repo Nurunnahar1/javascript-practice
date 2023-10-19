bd30e31f41c9fe9ecb1a569fd26cde0be454be63
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-19 20:12:19 +0600

    style binding

--- javascript practice/vuejs/Library/src/App.vue
+++ javascript practice/vuejs/Library/src/App.vue
@@ -1,71 +1,40 @@
 <script setup>
-const bgOrange = "bg-orange-400";
-const bgIndigo = "bg-indigo-400";
-const bgGreen = "bg-green-400";
-const textColor = "text-white";
+ const bgOrange = 'background-color:    rgb(251,146,60,1); color: whitte; font-size:24px'
+ const bgIndigo = 'background-color:    rgb(129,140,248, 1)'
+ const bgGreen = 'background-color:    rgb(52,211,153, 1)'
 
-const bgRed = "bg-red-600";
-const bgDarkGreen = "bg-green-600";
-const weekend = true;
 
-const colors = {
-    bgColor:'bg-teal-100',
-    borderColor:'border-teal-500',
-    textColor:'border-t-4'
-}
-
-const messageType = 'info';
-
-if('error' == messageType){
-    colors.bgColor='bg-red-100';
-    colors.textColor='text-red-900';
-    colors.borderColor='border-red-500';
-}
- 
-if('warning' == messageType){
-    colors.bgColor='bg-yellow-900';
-    colors.textColor='text-red-900';
-    colors.borderColor='border-red-500';
-}
-
-if('info' == messageType){
-    colors.bgColor='bg-green-300';
-    colors.textColor='text-red-900';
-    colors.borderColor='border-red-500';
-}
-
+ const bgRed = 'background-color: rgba(220,38,38,1)'
+ const bgDarkGreen = 'background-color: rgba(5,150,105,1)'
+ const weekday = true
 
 </script>
 
 <template>
-  <section class="container mx-auto">
-    <h1 class="text-center text-2xl py10">Class & Style Binding</h1>
-
-    <section class="flex space-x-10 mb-4">
-      <div class="w-1/2 h-16" :class="[bgOrange, textColor]">Hello</div>
-      <div class="w-1/2 h-16" :class="bgIndigo"></div>
-      <div class="w-1/2 h-16" :class="bgGreen"></div>
+    <section class="container mx-auto">
+      <h1 class="text-center text-3xl py-12">Class & Style Binding</h1>
+  
+      <section class="flex space-x-10">
+        <div class="w-1/3  h-16" :style="[bgOrange]">Hello I am Nasrin.......</div>
+        <div class="w-1/3  h-16" :style="[bgIndigo]"></div>
+        <div class="w-1/3  h-16" :style="[bgGreen]"></div>
+      </section>
+  
+      <section class="mt-10  ">
+        <div class=" h-16 flex items-center justify-center text-white" :style="weekday==true?bgRed:bgDarkGreen">
+          It's a weekday!
+        </div>
+      </section>
+  
+      <section class="mt-10 hidden">
+        <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+          <p class="font-bold">Our privacy policy has changed</p>
+          <p class="text-sm">Make sure you know how these changes affect you.</p>
+        </div>
+      </section>
     </section>
-
-    <section>
-      <div
-        class="h-16 flex items-center justify-center text-white"
-        :class="weekend == true ? bgDarkGreen : bgRed"
-      >
-        It's a weekday!
-      </div>
-    </section>
-    <section class="mt-10  ">
-      <div
-        class="  rounded-b text-teal-900 px-4 py-3 shadow-md"
-        role="alert"  :class="[colors.bgColor,colors.borderColor,colors.textColor]"
-      >
-        <p class="font-bold">Our privacy policy has changed</p>
-        <p class="text-sm">Make sure you know how these changes affect you.</p>
-      </div>
-    </section>
-  </section>
-</template>
+  </template>
+  
 
 <style scoped>
 </style>
