98de601635ddf6d9acd7d8f5ee28b5089c1386d9
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-21 18:30:00 +0600

    access function data & object data

---
+++ javascript practice/vuejs/module3/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+ const message = 'Welcome to vuejs 3'
+
+
+ //access method data
+ const date = '21-10-2023'
+ function getDate (){
+  const date = new Date();
+  // return date.getFullYear()
+  // return date.getDay()
+  // return date.getHours()
+  // return date.getMinutes()
+  // return date.getSeconds()
+  return date.toDateString()
+}
+
+
+  //get object data
+  const data ={
+  message:'Welcome to vuejs',
+  date:'21-10-2023',
+  tasks:[1,2,3,4,5]
+}
+</script>
+
+<template>
+ <h1>{{ message }}</h1>
+ <h3>{{ getDate() }}</h3>
+
+ <h1>{{ data.message }}</h1>
+ <h1>{{ data.date }}</h1>
+ <p>You have {{ data.tasks.length }} tasks</p>
+ <p>You have {{ data.tasks[2] }} tasks</p>
+ 
+</template>
+
+<style scoped>
+ 
+</style>
