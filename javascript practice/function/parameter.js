3bbf5c2c9e33d0f74951c639f720c35996428e5f
Nurunnahar <nurunnahar9081@gmail.com>
2023-12-28 21:44:10 +0600

    function

---
+++ javascript practice/function/parameter.js
@@ -0,0 +1,6 @@
+function sleep(human, designation) {
+  console.log("I am " + human + " I am a " + designation);
+}
+sleep("Nasrin", "Laravel Developer");
+sleep("Muskan", "Business wemen");
+sleep("Nurunnahar", "Householder");
