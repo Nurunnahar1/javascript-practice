5555b6cd227e3ad783cdb4d9b998ef1cd86bdc11
Nurunnahar <nurunnahar9081@gmail.com>
2023-12-29 07:06:53 +0600

    function

--- javascript practice/function/parameter.js
+++ javascript practice/function/parameter.js
@@ -1,6 +1,23 @@
+//way -1 ======================
+
 function sleep(human, designation) {
   console.log("I am " + human + " I am a " + designation);
 }
 sleep("Nasrin", "Laravel Developer");
 sleep("Muskan", "Business wemen");
 sleep("Nurunnahar", "Householder");
+
+
+
+//way - 2 ======================
+function myFunction(a, b) {
+  return (a * b);
+}
+let x = myFunction(5, 6);
+console.log(x);
+
+let y = myFunction(6, 6);
+console.log(y);
+
+console.log(myFunction(7, 7));
+myFunction(7, 7);  //this is wrong
