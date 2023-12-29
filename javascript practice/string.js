810fb8a50e98351b1dd851fe5bd814e293b903fd
Nurunnahar <nurunnahar9081@gmail.com>
2023-12-29 16:33:07 +0600

    function ,string

---
+++ javascript practice/string.js
@@ -0,0 +1,23 @@
+//===================1=================
+
+let nasrin = "I am Nasrin";
+let muskan = nasrin.replace("Nasrin", "Muskan");
+console.log(muskan);
+
+//===================2=================
+let text1 = "Good";
+let text2 = "Job";
+let text3 = text1.concat(" ", text2);
+console.log(text3);
+
+//===================3=================
+
+let a = "          Bangladesh           ";
+let c = a.trim();
+console.log(c);
+
+//===================4=================
+let x = "Hello world";
+console.log(x.split(","));
+console.log(x.split(" "));
+console.log(x.split(""));
