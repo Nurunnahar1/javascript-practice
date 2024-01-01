901c4faa631710fbb184ca7cf3a5ab5c400b6a04
Nurunnahar <nurunnahar9081@gmail.com>
2024-01-01 21:45:46 +0600

    how many vowels in a sentence

---
+++ javascript practice/problem&solution/problem-5.js
@@ -0,0 +1,15 @@
+//how many vowels in a sentence
+
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+function countVowels(sentence) {
+  let count = 0;
+  const letter = Array.from(sentence);
+  letter.forEach(function (value) {
+    if (vowels.includes(value)) {
+      count++;
+    }
+  });
+  return count;
+}
+console.log(countVowels("I Love Bangladesh because of its beauty"));
