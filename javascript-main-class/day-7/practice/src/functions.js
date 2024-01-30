16227e107b592ed2c31af2950c120764c9b33795
Nurunnahar <nurunnahar9081@gmail.com>
2024-01-30 21:25:21 +0600

    currencyConverter function

--- javascript-main-class/day-7/practice/src/functions.js
+++ javascript-main-class/day-7/practice/src/functions.js
@@ -4,35 +4,67 @@ function info(name) {
 
 /**
  * agr calculates
- * @param {*} name 
- * @param {*} year 
+ * @param {*} name
+ * @param {*} year
  */
 function dev(name, year) {
-     let age = 2024 - year;
-  console.log(`Hello, This is ${name}........ and i am ${age} years old........`);
+  let age = 2024 - year;
+  console.log(
+    `Hello, This is ${name}........ and i am ${age} years old........`
+  );
 }
-
 
 /**
  * marriage age calculation
- * @param {*} name 
- * @param {*} year 
- * @param {*} gender 
+ * @param {*} name
+ * @param {*} year
+ * @param {*} gender
  */
- 
+
 function marriageCalculator(name, year, gender = "male") {
-    let age = 2024 - year;
-    let marriageAge = 21;
+  let age = 2024 - year;
+  let marriageAge = 21;
 
-    if (gender === "female") {
-        marriageAge = 18 ;
-    }
-    if (age >= marriageAge) {
-        console.log(`Hello ${name} , You are ${age} years old and you will get marry......`)
-    } else {
-        console.log(
-          `Hello ${name} , You are ${age} years old and you will not  get marry now. You will get marry after ${marriageAge - age} years later.....`
-        );
+  if (gender === "female") {
+    marriageAge = 18;
+  }
+  if (age >= marriageAge) {
+    console.log(
+      `Hello ${name} , You are ${age} years old and you will get marry......`
+    );
+  } else {
+    console.log(
+      `Hello ${name} , You are ${age} years old and you will not  get marry now. You will get marry after ${
+        marriageAge - age
+      } years later.....`
+    );
+  }
+}
+
+/**
+ * currency converter
+ * @param {*} amount 
+ * @param {*} currencyType 
+ */
+
+function currencyConverter(amount, currencyType = "dollar") { 
+    let rate;
+    switch (currencyType) {
+      case "dollar":
+        rate = 109;
+        break;
+      case "euro":
+        rate = 120;
+        break;
+      case "won":
+        rate = 0.082;
+        break;
+      default:
+        rate = 109;
+        break;
     }
 
- }
+    console.log(`
+        ${amount} ${currencyType} = ${amount * rate} Taka
+    `)
+}
--- javascript-main-class/day-7/practice/src/main.js
+++ javascript-main-class/day-7/practice/src/main.js
@@ -1,4 +1,7 @@
 info('Nasrin');
+
 dev('Muskan', 1997);
  
-marriageCalculator('Nuha',2013, 'female');
+marriageCalculator('Nuha', 2013, 'female');
+
+currencyConverter(500, 'dollar');
