8c3abd8f2d4804ad9a8c65874d63a98e21c7ea89
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-03 22:02:41 +0600

    three different way of function dicration

--- javascript-main-class/day-8(function)/practice/src/functions.js
+++ javascript-main-class/day-8(function)/practice/src/functions.js
@@ -7,5 +7,22 @@
 function ageCal(name, year) {
   let age = 2024 - year;
 
-  console.log(`Hello ${name}, Your are ${age} years old`);
+  return `Hello ${name}, Your are ${age} years old`;
+};
+
+
+
+let ageCal2 = function (name, year) {
+    let age = 2024 - year;
+    
+    return `Hello ${name}, Your are ${age} years old`;
+};
+
+
+
+
+let ageCal3 = (name, year) => {
+    let age = 2024 - year;
+    return `Hello ${name}, Your are ${age} years old`;
 }
+ 
--- javascript-main-class/day-8(function)/practice/src/main.js
+++ javascript-main-class/day-8(function)/practice/src/main.js
@@ -1,1 +1,8 @@
-ageCal("Nasrin", 1997);
+// ageCal("Nasrin", 1997);
+ageCal2("Muskan", 1997);
+ageCal3("Nuha", 2013);
+
+
+console.log(ageCal("Nasrin", 1997));
+console.log(ageCal2("Muskan", 1997));
+console.log(ageCal3("Nuha", 2013));
