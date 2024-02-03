83823e643231ea6d34f53a3c7b0842feaa87ce2c
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-03 23:15:56 +0600

    function

--- javascript-main-class/day-8(function)/practice/src/functions.js
+++ javascript-main-class/day-8(function)/practice/src/functions.js
@@ -26,3 +26,39 @@ let ageCal3 = (name, year) => {
     return `Hello ${name}, Your are ${age} years old`;
 }
  
+
+
+/**
+ * currency converter
+ * @param {*} amount 
+ * @param {*} currencyType 
+ */
+
+let currencyConverter = (amount , currencyType= 'dollar') => {
+    let rate;
+
+    switch (currencyType) {
+        case 'dollar':
+            rate = 109;
+            break;
+        case 'euro':
+            rate = 119;
+            break;
+        case 'pound':
+            rate = 139;
+            break;
+        case 'won':
+            rate = 0.08;
+            break;
+        case 'rupe':
+            rate = 1.32;
+            break;
+        default:
+            rate = 109;
+            break;
+        
+    }
+
+    let bdt = amount * rate;
+    return bdt;
+}
--- javascript-main-class/day-8(function)/practice/src/main.js
+++ javascript-main-class/day-8(function)/practice/src/main.js
@@ -1,8 +1,7 @@
-// ageCal("Nasrin", 1997);
-ageCal2("Muskan", 1997);
-ageCal3("Nuha", 2013);
-
-
+ 
 console.log(ageCal("Nasrin", 1997));
 console.log(ageCal2("Muskan", 1997));
 console.log(ageCal3("Nuha", 2013));
+
+
+console.log(`Hello, 300 dollar = ${currencyConverter(300, "won")}`);
