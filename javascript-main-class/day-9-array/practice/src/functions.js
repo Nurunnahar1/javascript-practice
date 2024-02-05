38aa8d354a9dabb8a0f353b8174859ac71c09eec
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-05 14:53:58 +0600

    array

---
+++ javascript-main-class/day-9-array/practice/src/functions.js
@@ -0,0 +1,14 @@
+const name = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG"];
+const age = [8, 9, 5, 22, 30, 70, 80];
+const location = ["Dhaka", "Rajshahi", "Rangpur", "Bogura", "Dhaka", "Mymansing", "Sherpur"];
+
+for (let i = 0; i < name.length; i++) { 
+  console.log(name[i]);
+}
+for (let i = 0; i < age.length; i++) {
+  console.log(age[i]);
+}
+for (let i = 0; i < location.length; i++) {
+  console.log(location[i]);
+}
+ 
