178c7ec449d44647791785a74d4dd13a0005885c
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-05 22:28:58 +0600

    push, pop, shift, unshift

--- javascript-main-class/day-9-array/practice/src/functions.js
+++ javascript-main-class/day-9-array/practice/src/functions.js
@@ -1,17 +1,27 @@
-const name = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG"];
-const age = [8, 9, 5, 52, 30, 70, 80];
-const location = [
-  "Dhaka",
-  "Rajshahi",
-  "Rangpur",
-  "Rajshahi",
-  "Dhaka",
-  "Mymansing",
-  "Mymansing",
-];
+// const name = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG"];
+// const age = [8, 9, 5, 52, 30, 70, 80];
+// const location = [
+//   "Dhaka",
+//   "Rajshahi",
+//   "Rangpur",
+//   "Rajshahi",
+//   "Dhaka",
+//   "Mymansing",
+//   "Mymansing",
+// ];
 
-for (let i = 0; i < name.length; i++) {
-  if (location[i] != "Rajshahi" && age[i] <40) {
-    console.log(`Hello ${name[i]}, You live in Rajshahi `);
-  }
-}
+// for (let i = 0; i < name.length; i++) {
+//   if (location[i] != "Rajshahi" && age[i] <40) {
+//     console.log(`Hello ${name[i]}, You live in Rajshahi `);
+//   }
+// }
+
+
+
+
+const name = ['A', 'B', 'C', 'D', 'E'];
+name.push('F');
+name.unshift('O');
+name.pop(name);
+name.shift()
+console.log(name);
