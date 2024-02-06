69c21963b351028573bfe5328f451e8ee82add19
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-06 13:54:32 +0600

    concate()

--- javascript-main-class/day-10-array/practice/src/functions.js
+++ javascript-main-class/day-10-array/practice/src/functions.js
@@ -1,5 +1,8 @@
 const food = ["potato", "brinjal", "pumpkin", "cucumber"];
+const fruits = ["Apple", "Orange", "Pineapple", "Jackfruit"];
  
-for (let i = food.length - 1; i >= 0; i--){
-  console.log(food[i]);
-}
+// for (let i = food.length - 1; i >= 0; i--){
+//   console.log(food[i]);
+// }
+
+console.log(food.concat(fruits));
