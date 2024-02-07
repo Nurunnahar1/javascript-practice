d3aa23abe6c850144c21da1e622f7630b7ab432b
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-07 21:46:19 +0600

    findIndex , indexOf

--- javascript-main-class/day-10-array/practice/src/functions.js
+++ javascript-main-class/day-10-array/practice/src/functions.js
@@ -10,13 +10,25 @@
 // console.log(Array.isArray(food));
 
 //=================================
-const food = ["potato", "brinjal", "pumpkin", "cucumber"];
-const foodSlice = food.slice(0, 1);
-const foodSlice1 = food.slice(3, 4);
+// const food = ["potato", "brinjal", "pumpkin", "cucumber"];
+// const foodSlice = food.slice(0, 1);
+// const foodSlice1 = food.slice(3, 4);
 
 // const foodAndFoodSlice = foodSlice.concat(foodSlice1);
-console.log(foodSlice.concat(foodSlice1));
+// console.log(foodSlice.concat(foodSlice1));
 
-const foodSplice = food.splice(1, 2);
-console.log(foodSplice);
-console.log(food);
+// const foodSplice = food.splice(1, 2);
+// console.log(foodSplice);
+// console.log(food);
+//=================================
+    // const foods = ["potato", "brinjal", "pumpkin", "cucumber"]; 
+    // foods.splice(1, 0, "Cabbage", "Coliflower");
+    // console.log(foods);
+
+
+
+
+//=================================
+const foods = ["potato", "brinjal", "pumpkin", "cucumber"];
+// console.log(foods.findIndex((item) =>  item== "cucumber"));
+console.log(foods.indexOf('potato'));
