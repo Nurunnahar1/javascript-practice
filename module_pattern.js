0bc95dcfeef844d23fb7f52bbbe5cb0cb509e96c
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-12 23:15:55 +0600

    Module Prototype

---
+++ module_pattern.js
@@ -0,0 +1,40 @@
+function AnimalContainer() {
+    const container = [];
+
+    function getAllAnimals() {
+        return container;
+    }
+
+    function addAnimal(name) {
+        container.push(name);
+    }
+
+    function removeAnimal(name) { 
+        const index = container.indexOf(name);
+        if (index < 1) {
+            throw new Error(`${name} not found.`);
+        }
+        container.splice(index, 1);
+    }
+
+
+    return {
+        add: addAnimal,
+        get: getAllAnimals, 
+        remove:removeAnimal
+    }
+
+}
+
+const zooContainer = AnimalContainer();
+
+// zooContainer.add(["Lion", "Tiger", "Snake", "Monkey"]);
+
+zooContainer.add("Lion");
+zooContainer.add("Tiger");
+zooContainer.add("Snake");
+zooContainer.add("Monkey");
+
+
+console.log(zooContainer.get());
+zooContainer.remove("Sheep");
