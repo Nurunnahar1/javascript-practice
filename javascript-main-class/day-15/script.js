29e414b626f340a966952e2a80d95587fa82c682
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-24 11:37:52 +0600

    assignment 14 (How can i store back objectpattern)

--- javascript-main-class/day-15/script.js
+++ javascript-main-class/day-15/script.js
@@ -1,11 +1,39 @@
-let devs = {
-  name: "Nasrin",
-  age: 28,
-  skill: "Laravel Developer",
-};
+let devs = [
+  {
+    name: "Nasrin",
+    age: 28,
+    skill: "Laravel Developer",
+  },
+  {
+    name: "Moriom",
+    age: 15,
+    skill: "Frontend Developer",
+  },
+  {
+    name: "Nuha",
+    age: 10,
+    skill: "Laravel Developer",
+  },
+  {
+    name: "Muskan",
+    age: 45,
+    skill: "Backend Developer",
+  },
+  {
+    name: "Shultana",
+    age: 28,
+    skill: "MERN  ",
+  },
+];
+ 
 
-// localStorage.setItem("dev", devs);   //this is not work
+// console.log(JSON.stringify(devs));
+ localStorage.setItem("devs", JSON.stringify(devs));
+ console.log(devs)
 
-// const dev = localStorage.getItem("devs");
 
-console.log(JSON.stringify(devs));
+let dev = localStorage.getItem("devs");
+// console.log(dev);
+console.log(JSON.parse(dev));
+ 
+ 
