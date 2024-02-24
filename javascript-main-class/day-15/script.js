9ae3e35a8e6475bfff3e16cbcb5be8098e775b1c
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-24 11:18:44 +0600

    assignment 14 (How can i store any object in browser local storage)

---
+++ javascript-main-class/day-15/script.js
@@ -0,0 +1,11 @@
+let devs = {
+  name: "Nasrin",
+  age: 28,
+  skill: "Laravel Developer",
+};
+
+// localStorage.setItem("dev", devs);   //this is not work
+
+// const dev = localStorage.getItem("devs");
+
+console.log(JSON.stringify(devs));
---
+++ javascript-main-class/day-15/src/functions.js
@@ -0,0 +1,14 @@
+ localStorage.setItem("name", "Nasrin");
+ localStorage.setItem("age", 28);
+localStorage.setItem("skill", "Laravel Developer");
+ 
+console.log(localStorage);
+// console.log(localStorage.getItem("name"));
+console.log(localStorage.removeItem("name"));
+
+
+//=============setItem,   getItem,   removeItem============
+
+//sessionStorage
+
+sessionStorage.setItem("name", "Nurunnahar");
