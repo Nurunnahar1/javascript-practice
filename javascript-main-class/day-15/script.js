487f9c40619dec16a70b82ed6e5d6b4985b0adb7
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-25 22:15:14 +0600

    Local storage

--- javascript-main-class/day-15/script.js
+++ javascript-main-class/day-15/script.js
@@ -1,39 +1,27 @@
-let devs = [
+const devs = [
   {
     name: "Nasrin",
     age: 28,
     skill: "Laravel Developer",
   },
   {
-    name: "Moriom",
-    age: 15,
-    skill: "Frontend Developer",
+    name: "Nurunnahar",
+    age: 50,
+    skill: "IOS Developer",
   },
   {
-    name: "Nuha",
-    age: 10,
+    name: "Moriom",
+    age: 11,
     skill: "Laravel Developer",
   },
   {
-    name: "Muskan",
-    age: 45,
-    skill: "Backend Developer",
-  },
-  {
-    name: "Shultana",
+    name: "Nuha",
     age: 28,
-    skill: "MERN  ",
+    skill: "IOS Developer",
   },
 ];
- 
 
-// console.log(JSON.stringify(devs));
- localStorage.setItem("devs", JSON.stringify(devs));
- console.log(devs)
+// localStorage.setItem("devs", JSON.stringify(devs));
+let dev = localStorage.getItem("devs");         
 
-
-let dev = localStorage.getItem("devs");
-// console.log(dev);
-console.log(JSON.parse(dev));
- 
- 
+let devData = JSON.parse(dev);
