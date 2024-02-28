6ce8cb2baeb85ca882a6ffbae8f0a25ed5412043
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-28 11:22:54 +0600

    Local storage (setItem, getItem)

--- javascript-main-class/day-15/script.js
+++ javascript-main-class/day-15/script.js
@@ -1,26 +1,2 @@
-const devs = [
-  {
-    name: "Nasrin",
-    age: 28,
-    skill: "Laravel Developer",
-  },
-  {
-    name: "Nurunnahar",
-    age: 50,
-    skill: "IOS Developer",
-  },
-  {
-    name: "Moriom",
-    age: 11,
-    skill: "Laravel Developer",
-  },
-  {
-    name: "Nuha",
-    age: 28,
-    skill: "IOS Developer",
-  },
-];
-
-let dev = localStorage.getItem("devs");
-
-let devData = JSON.parse(dev);
+//  localStorage.setItem("name", "Nasrin");
+console.log(localStorage.getItem("name"))
