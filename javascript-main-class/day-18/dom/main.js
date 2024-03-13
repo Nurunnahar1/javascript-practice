98d6a68363e50ba5d2037a045fc0865b1ceb74de
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-13 09:02:31 +0600

    nextElementSibling, lastElementChild, firstElementChild

--- javascript-main-class/day-18/dom/main.js
+++ javascript-main-class/day-18/dom/main.js
@@ -1,2 +1,13 @@
-console.log(document.querySelector("ul"));
-console.log(document.querySelector("ul").children);
+// console.log(document.querySelector("ul"));
+// console.log(document.querySelector("ul").children);
+
+
+// const child = document.querySelector("ul");
+// console.log(child.firstElementChild);
+// console.log(child.lastElementChild);
+
+
+
+const child = document.querySelector(".box");
+
+console.log(child.firstElementChild.nextElementSibling); 
