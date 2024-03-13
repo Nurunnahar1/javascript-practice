f9ae65798437fb88b2704beb2e59082fedb8ea83
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-13 10:08:16 +0600

    addEventListener

--- javascript-main-class/day-18/dom/main.js
+++ javascript-main-class/day-18/dom/main.js
@@ -1,17 +1,5 @@
-// console.log(document.querySelector("ul"));
-// console.log(document.querySelector("ul").children);
+const btn = document.querySelector("button");
 
-// const child = document.querySelector("ul");
-// console.log(child.firstElementChild);
-// console.log(child.lastElementChild);
-
-const child = document.querySelector(".box p:nth-child(4)");
-console.log(child.parentElement);
-
-
-child.style.backgroundColor = "red";
-child.nextElementSibling.style.backgroundColor = "green";
-child.previousElementSibling.style.backgroundColor = "purple";
-child.parentElement.style.backgroundColor = "blue";
-
-// console.log(child.firstElementChild.nextElementSibling);
+btn.addEventListener("click", function () {
+    alert("Hello!");
+});
