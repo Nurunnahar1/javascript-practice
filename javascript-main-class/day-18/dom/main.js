7789d7f105524a5f6fa84cb16b9060ccd802add0
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-13 10:47:02 +0600

    keypress

--- javascript-main-class/day-18/dom/main.js
+++ javascript-main-class/day-18/dom/main.js
@@ -1,5 +1,22 @@
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-btn.addEventListener("click", function () {
+// btn.addEventListener("click", function () {
+//     alert("Hello!");
+// });
+// btn.addEventListener("mouseenter", function () {
+//     alert("Hello!");
+// });
+// btn.addEventListener("mousedown", function () {
+//     alert("Hello!");
+// });
+
+
+
+ 
+// input.addEventListener("keypress", function () {
+//     alert("Hello!");
+// })
+const input = document.querySelector("input");
+input.addEventListener("keypress", () => {
     alert("Hello!");
 });
