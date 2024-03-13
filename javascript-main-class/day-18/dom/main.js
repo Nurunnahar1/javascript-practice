bb65345e57816f375aa72adbc767837d2e821701
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-13 09:29:32 +0600

    parentElement, previousElementSibling, nextElementSibling, style

--- javascript-main-class/day-18/dom/main.js
+++ javascript-main-class/day-18/dom/main.js
@@ -1,14 +1,17 @@
 // console.log(document.querySelector("ul"));
 // console.log(document.querySelector("ul").children);
 
-
 // const child = document.querySelector("ul");
 // console.log(child.firstElementChild);
 // console.log(child.lastElementChild);
 
-
-
 const child = document.querySelector(".box p:nth-child(4)");
-console.log(child);
+console.log(child.parentElement);
 
-// console.log(child.firstElementChild.nextElementSibling); 
+
+child.style.backgroundColor = "red";
+child.nextElementSibling.style.backgroundColor = "green";
+child.previousElementSibling.style.backgroundColor = "purple";
+child.parentElement.style.backgroundColor = "blue";
+
+// console.log(child.firstElementChild.nextElementSibling);
