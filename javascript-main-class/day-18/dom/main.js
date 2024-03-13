1509e7e5cef69e2004c9737f4ccdfcda8c8d630e
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-13 21:21:17 +0600

    keypress, keyup, keydown

--- javascript-main-class/day-18/dom/main.js
+++ javascript-main-class/day-18/dom/main.js
@@ -1,22 +1,7 @@
-// const btn = document.querySelector("button");
-
-// btn.addEventListener("click", function () {
-//     alert("Hello!");
-// });
-// btn.addEventListener("mouseenter", function () {
-//     alert("Hello!");
-// });
-// btn.addEventListener("mousedown", function () {
-//     alert("Hello!");
-// });
-
-
-
+const input = document.querySelector('input');
  
-// input.addEventListener("keypress", function () {
-//     alert("Hello!");
-// })
-const input = document.querySelector("input");
-input.addEventListener("keypress", () => {
-    alert("Hello!");
-});
+input.addEventListener('keypress', () => {
+    // console.log(input.value);
+    document.querySelector(".box h1").innerHTML = input.value;
+})
+
