98df168a512904946c6ce6156c07f3ed89718557
Nurunnahar <nurunnahar9081@gmail.com>
2024-04-17 15:34:46 +0600

    stop watch

--- javascript-main-class/dom-with-bom/src/main.js
+++ javascript-main-class/dom-with-bom/src/main.js
@@ -1,15 +1,32 @@
- const startButton = document.querySelector('.box .start');
- const stopButton = document.querySelector('.box .stop');
-const heading = document.querySelector('.box h2');
+const startButton = document.querySelector(".box .start");
+const stopButton = document.querySelector(".box .stop");
+const heading = document.querySelector(".box h2");
 
 let kaj;
+let count = 0;
+
+// startButton.onclick = (e) => {
+//     // kaj = setTimeout(() => {
+//     kaj = setInterval(() => {
+//         heading.innerHTML += count;
+//         count++;
+//      },1000);
+// }
+
+
+startButton.onclick = (e) => { 
+    kaj = setInterval(() => {
+
+        if (count >= 5) {
+            clearInterval(kaj);
+            heading.innerHTML = "0";
+        }
+        heading.innerHTML = count;
+        count++;
+     },1000);
+}
  
-startButton.onclick = (e) => {
- kaj = setTimeout(() => {
-    heading.innerHTML = "Hello........";
-  }, 1000);
-};
-stopButton.onclick = (e) => {
+stopButton.onclick = () => {
     clearTimeout(kaj);
-    heading.innerHTML = "Stop........";
-};
+    heading.innerHTML = "Stop.....";
+ }
